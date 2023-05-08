051bd9d7fa2a9f36f10f544280ebdaf724db911e
Hami12011 <98381490+Hami12011@users.noreply.github.com>
2023-05-08 15:39:55 +0500

    Add files via upload

--- random_update.py
+++ random_update.py
@@ -96,13 +96,8 @@ ses=requests.Session()
 princp=[]
 twf =[]
 user=[]
-ugen=[]
-for nt in range(10000):
-    rr=random.randint
-    aZ=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
-    rx=random.randrange(1, 999)
-    xx=f"Mozilla/5.0 (Windows NT 10.0; {str(rr(9,11))}.1.0; Win64; x64){str(aZ)}{str(rx)}{str(aZ)}) AppleWebKit/537.36 (KHTML, like Gecko){str(rr(99,149))}.0.{str(rr(4500,4999))}.{str(rr(35,99))} Chrome/{str(rr(99,175))}.0.{str(rr(0,5))}.{str(rr(0,5))} Safari/537.36"
-    ugen.append(xx)
+aqib_ua=[]
+
 
 loop = 0
 cp = []
